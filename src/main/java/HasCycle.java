0688f49a2d304b6dac3e81136a56abfbd32d01ac
chenjing <chenjing@wxblockchain.com>
2018-11-27 20:44:48 +0800

    添加判断单链表中是否有环的练习

---
+++ src/main/java/HasCycle.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class HasCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if(head == null||head.next==null){
+            return false;
+        }
+        ListNode temp1 = head;
+        HashMap<ListNode,ListNode> ns = new HashMap<ListNode,ListNode>();
+        while(head!=null){
+            if(ns.get(temp1)!=null)return true;
+            else ns.put(temp1, temp1);
+            temp1 = temp1.next;
+            if(temp1 == null)return false;
+        }
+        return true;
+    }
+
+}
