7a581dac1ba7742d681882b548b77e05fb1e6ccf
joiechen <joie_chen9@hotmail.com>
2019-03-14 22:20:44 +0800

    Add more for interview

--- src/main/java/SubArray.java
+++ src/main/java/SubArray.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubArray {
 
+    /***
+     * 给定和，从给定数组中找出等于这个和的子数组
+     * @param num
+     * @param sum
+     */
     public void findSubArray(int[] num,int sum){
         int left = 0;
         int right = 0;
@@ -12,10 +20,62 @@ public class SubArray {
             }
             if(curSum == sum){
                 for(int j=left;j<right;j++){
-                    System.out.print(num[j] + "");
+                    System.out.println(num[j] + "");
                 }
                 System.out.println();
             }
         }
     }
+
+    /***
+     * 求给定数组的最大和子数组
+     * @param params
+     * @return
+     */
+    public List<Integer> subArraySumItem(int[] params) {
+        int currentSum = 0;
+        int maxSum = 0;
+        int curStart = 0;
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < params.length; i++) {
+            currentSum += params[i];
+            if(currentSum > 0) {
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                    start = curStart ;
+                    end = i;
+                }
+            }else{
+                currentSum = 0;
+                curStart = i ;
+            }
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = start+1; i <=end ; i++) {
+            list.add(params[i]);
+        }
+        return list;
+    }
+
+    /***
+     * 获取数组和
+     * @param arr
+     * @return
+     */
+    public int getMaxSum(int[] arr) {
+        int max = -100000;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (max < sum) {
+                    max = sum;
+                }
+            }
+        }
+
+        return max;
+    }
+
 }
--- src/test/java/TestMethod.java
+++ src/test/java/TestMethod.java
@@ -1,21 +1,23 @@
 import org.junit.Test;
 
+import java.util.List;
+
 public class TestMethod {
 
     SubArray subarray = new SubArray();
-    SubArray01 subArray01 = new SubArray01();
     ReverseList reverselist = new ReverseList();
     ListNode listnode = new ListNode(6);
     BinarySearch binarySearch = new BinarySearch();
 
     @Test
     public void TestSubArray() {
-        int[] inputNum = {-1,0,1,2,3,4,6,7,8,8,10,15,16,17};
-        //System.out.print("test result---" + subArray01.sumOfSubArray(inputNum,16));
-        subarray.findSubArray(inputNum,16);
+        int[] inputNum = {-2, 2, -3, 4, -1, 2, 1, -5, 3};
+        subarray.findSubArray(inputNum,6);
+        System.out.println(subarray.subArraySumItem(inputNum));
+        System.out.println(subarray.getMaxSum(inputNum));
     }
 
-    @Test
+    /***@Test
     public void TestListNode() {
         System.out.print(reverselist.reverseList(listnode));
     }
@@ -24,5 +26,5 @@ public class TestMethod {
     public void TestBinarySearch(){
         int[] test = {1};
         System.out.print(binarySearch.binarySearch(test,1));
-    }
+    }***/
 }
